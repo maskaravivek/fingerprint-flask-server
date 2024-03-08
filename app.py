1cb21821846497d9f3a19d8fbcce539dd7525ada
Vivek Kumar Maskara <maskaravivek@gmail.com>
2024-03-08 06:37:23 -0800

    Update api key

--- app.py
+++ app.py
@@ -17,7 +17,7 @@ max_request_lifespan = 60 * 5 # 5 minutes
 visitor_id_rate_limit = 5
 
 # init the server API SDK
-configuration = fingerprint_pro_server_api_sdk.Configuration(api_key="Bt3HJ40j0sNB94QXPoWB")
+configuration = fingerprint_pro_server_api_sdk.Configuration(api_key="KOyxHqXby7t3tq9iAilm")
 api_instance = fingerprint_pro_server_api_sdk.FingerprintApi(configuration)
 
 @app.route('/')
